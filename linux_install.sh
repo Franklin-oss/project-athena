f8d32fa035624251de709782dd0228022676633e
LaFrnksta356 <65860526+Franklin-oss@users.noreply.github.com>
2021-09-16 12:12:51 +0530

    Added dependencies for pdftotext
    pdftotext cant be built directly from pip so added line to apt-get dependencies manually

--- linux_install.sh
+++ linux_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pip3 install -r requirements.txt
+sudo apt-get update
+sudo apt-get -y install build-essential libpoppler-cpp-dev pkg-config python-dev
 sudo apt-get -y install python3-tk  # Only works for debian systems
