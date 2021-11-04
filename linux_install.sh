68b44dbc80ca4f0f33a949ef43a523ac6923aed4
LaFrnksta356 <65860526+Franklin-oss@users.noreply.github.com>
2021-11-04 18:35:40 +0530

    Add python3-pip in dependencies

--- linux_install.sh
+++ linux_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install build-essential libpoppler-cpp-dev pkg-config python-dev
+sudo apt-get -y install build-essential libpoppler-cpp-dev pkg-config python-dev python3-pip
 sudo apt-get -y install python3-tk  # Only works for debian systems
 pip3 install -r requirements.txt
